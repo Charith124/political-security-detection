12623a8116c53268790c17858fa7035344a6dc0f
Charith124 <117744901+Charith124@users.noreply.github.com>
2026-02-19 21:16:42 +0530

    Add files via upload

---
+++ Pack Install.bat
@@ -0,0 +1,12 @@
+pip install numpy==1.19.2
+pip install pandas==0.25.3
+pip install matplotlib==3.1.1
+pip install jupyter==1.0.0
+pip install scikit-learn==0.22.2.post1
+pip install seaborn==0.10.1
+pip install ipython==7.9.0
+pip install ipython-genutils==0.2.0
+pip install ipykernel==6.5.0
+pip install nltk==3.4.5
+pip install NRCLex==3.0.0
+pause
